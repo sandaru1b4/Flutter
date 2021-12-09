b6e99210f5ba80059cc12785c8719c57a22b624f
Achitha Sandaruwan <sandaruwanb4@gmail.com>
2021-12-10 01:04:09 +0530

    initial commmit

---
+++ lib/main.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:device_preview/device_preview.dart';
+
+void main() {
+  runApp(
+      DevicePreview(
+        enabled: !kReleaseMode,
+        builder: (context) => MyApp(), // Wrap your app
+      ),
+  );
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      useInheritedMediaQuery: true,// Set to true
+      locale: DevicePreview.locale(context), // Add the locale here
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text("My Title"),
+        ),
+        body: Center(
+          child: Text(
+            "Hello world",
+            style: TextStyle(fontSize: 25.0),
+          ),
+        ),
+      ),
+    );
+  }
+}
